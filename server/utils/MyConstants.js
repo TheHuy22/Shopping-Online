f3520007a57166c7e6b5b3a60fd3e06856eeac44
TheHuy22 <139510944+TheHuy22@users.noreply.github.com>
2023-07-14 12:55:49 +0700

    add project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'atlascluster.m1wl9k1.mongodb.net',
+    DB_USER: 'TheHuy',
+    DB_PASS: 'nguyentranthehuy2210',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: '<email_user>', // Microsoft mail service
+    EMAIL_PASS: '<email_pass>',
+    JWT_SECRET: 'jwt_secret',
+    JWT_EXPIRES: '999999999999', // in milliseconds
+  };
+  module.exports = MyConstants;
